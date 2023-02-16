3f96bab4ae5686de082d4fb7bfcfcde99fe0181e
trung <leevantrung2004@gmail.com>
2023-02-16 21:34:15 +0700

    2/16/2023

---
+++ tinhchanle.cpp
@@ -0,0 +1,11 @@
+// Viết chương trình nhập vào một số nguyên dương x
+// . In ra chữ YES nếu x
+//  là số chẵn, ngược lại in số chữ NO.
+
+// Dữ liệu vào
+
+// Một số nguyên dương x
+// Kết quả
+
+// In ra chữ YES nếu x
+//  là số chẵn, ngược lại in số chữ NO.
